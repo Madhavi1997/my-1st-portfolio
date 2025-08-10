646830da0970acd4e1f9c0036c3a0b4d8a51ff95
Madhavi1997 <thakshiladissanayake3@gmail.com>
2025-08-10 23:25:10 +0530

    Semi Final

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing",{
+    strings:["Software Engineer","Web Designer","Web Developer","Graphic Designer","You Tuber","UI UX Designer","QA Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
